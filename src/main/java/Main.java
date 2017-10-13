71f8398a548ed3708b06fcdf384b9da8d67d1e9f
Khabenko <m.habenko@ukr.net>
2017-10-13 21:11:34 +0300

    Commit Home

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,8 @@ public class Main {
        list = conector.selectResultList("SELECT Name FROM Contact","Name");
         System.out.println(list.size());
         list = conector.selectResultList("SELECT Name FROM Contact WHERE Name = 'Oleg Mysterious Uknowovych';","Name");
-        System.out.println(list.size());
 
+       System.out.println(list.size());
 
 
     }
