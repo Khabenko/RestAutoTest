824022702599cd8776de8f2ea49457c8d34d12da
Khabenko <m.habenko@ukr.net>
2017-10-13 21:05:44 +0300

    Commit Home

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         DBConector conector = new DBConector();
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list;
 
-        list = conector.selectResultList("SELECT Name FROM Contact","Name");
+       list = conector.selectResultList("SELECT Name FROM Contact","Name");
         System.out.println(list.size());
         list = conector.selectResultList("SELECT Name FROM Contact WHERE Name = 'Oleg Mysterious Uknowovych';","Name");
         System.out.println(list.size());
+
 
 
     }
