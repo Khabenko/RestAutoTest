82efd9d0ce8a51c5e276cc4272694880e9214381
mkhabenko <31030147+mkhabenko@users.noreply.github.com>
2017-10-13 18:54:57 +0300

    DBConector

--- src/main/java/DBConector.java
+++ src/main/java/DBConector.java
@@ -1,22 +1,21 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class DBConector {
-
-    static String  DB_CONNECTION ="jdbc:mysql://10.0.14.152:3306/Tavriav790_T";
+    static String  DB_CONNECTION ="jdbc:mysql://10.0.14.152:3306/Tavriav790_T?autoReconnect=true&useSSL=false";
     static String  DB_USER ="Supervisor";
     static String  DB_PASSWORD ="Supervisor";
 
+
     public static Connection getDBConnection() {
         Connection dbConnection = null;
-  /*     try {
-            Class.forName(DB_DRIVER);
+      try {
+            Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.out.println("Where is your SQL JDBC Driver?");
             System.out.println(e.getMessage());
         }
-*/
+
         try {
             dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,DB_PASSWORD);
             return dbConnection;
@@ -25,5 +24,40 @@ public class DBConector {
         }
         return dbConnection;
     }
+
+
+    public  ArrayList<String> selectResultList (String query, String returnColumn)
+    {
+
+
+        //    String selectTableSQL = "SELECT top 1 Id from CashPointUA_T_R.dbo.Contact order by CreatedOn desc";
+        ArrayList<String> list = new ArrayList<>();
+        String selectTableSQL = query ;
+     //   selectTableSQL ="SELECT Name FROM Contact WHERE Name = 'Oleg Mysterious Uknowovych';";
+        Statement statement ;
+        DBConector conector = new DBConector();
+        try {
+            System.out.println(DBConector.getDBConnection());
+            System.out.println("---------------Ответ SQL------------------");
+            statement = conector.getDBConnection().createStatement();
+            ResultSet rs = statement.executeQuery(selectTableSQL);
+            // И если что то было получено то цикл while сработает
+            while (rs.next()) {
+                String result = rs.getString(returnColumn);
+                list.add(result);
+                System.out.println("Result: " + result);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("------------------------------------------");
+
+        return list;
+    }
+
+
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        DBConector conector = new DBConector();
+        ArrayList<String> list = new ArrayList<>();
+
+       list = conector.selectResultList("SELECT Name FROM Contact","Name");
+        System.out.println(list.size());
+        list = conector.selectResultList("SELECT Name FROM Contact WHERE Name = 'Oleg Mysterious Uknowovych';","Name");
+
+
+
     }
+
+
+
+
 }
